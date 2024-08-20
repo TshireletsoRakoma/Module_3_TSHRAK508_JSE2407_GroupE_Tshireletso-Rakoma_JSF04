05407f28aecd22079ea1d20b0451bbbfd968fecd
TshireletsoRakoma <156686384+TshireletsoRakoma@users.noreply.github.com>
2024-08-20 22:39:10 +0200

    "Added OrderHistory.vue component with template, script, and style blocks"

---
+++ vite-project/src/components/OrderHistory.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="order-history">
+      <h1>Order History</h1>
+      <ul>
+        <li v-for="order in pastOrders" :key="order.id">
+          Order ID: {{ order.id }} - Total: ${{ order.total }}
+          <button @click="cancelOrder(order.id)">Cancel Order</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import { mapGetters, mapActions } from 'vuex';
+  
+  export default {
+    name: "OrderHistory",
+    computed: {
+      ...mapGetters(['pastOrders']),
+    },
+    methods: {
+      ...mapActions(['cancelOrder']),
+    }
+  };
+  </script>
+  
+  <style scoped>
+  /* styles for order history */
+  </style>
+  
